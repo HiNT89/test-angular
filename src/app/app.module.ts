47c7290112b3469abed1ca315097d261588141f5
HiNT89 <hieunt89@gmail.com>
2024-03-04 20:43:41 +0700

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {fakeBackendProvider} from './core/services/fake-backend';
+import {AccountService} from './core/services/account.service';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    // provider used to create fake backend,
+    AccountService,
+    fakeBackendProvider
+  ]
+})
+export class AppModule {
+}
